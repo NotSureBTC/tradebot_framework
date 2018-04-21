f3af1953ccbc1501b77d28e3da355679f14f24c5
Not Sure <notsurebtc@gmail.com>
2018-04-21 09:29:45 -0600

    sample trade bot added

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ Requirements:
  * requests
  * numpy
  * tulipy
+
+# Sample Trade Bot
+
+*THIS BOT IMPLEMENTS A SIMPLE M15 KVO / SIGNAL STRATEGY THAT WILL PROBABLY LOSE MONEY! USE AT YOUR OWN RISK*
+
+To use the sample trade bot, copy config.py.example to config.py and put in your BitMEX credentials. If you use testnet credentials make sure to set the bitmex test setting to "True".
+
+You should set stoplosses in the config file as a percentage and the bot will bracket your trade with a stop market / limit order stoplooss / take profit bracket, which will be updated based on entry price automatically.
+
+Finally, you can add Twilio credentials to have the bot text you when it trades.
+
+I recommend running the bot in screen like ./tradebot.py. It will output info to the console, and will log extensive debug information in full.log, and log trades in trades.log.
---
+++ tradebot.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python2
+# coding=utf-8
+from __future__ import print_function
+import time
+import logging
+#logging.basicConfig(level=logging.INFO)
+import mexorders
+from indicators import *
+from notifications import send_sms
+from utilities import *
+import ExchgData
+
+import config
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+dh = logging.FileHandler('full.log')
+dh.setLevel(logging.DEBUG)
+ih = logging.FileHandler('tradebot.log')
+ih.setLevel(logging.INFO)
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+ffm = logging.Formatter('[%(asctime)s][%(levelname)s][%(name)s] %(message)s')
+cfm = logging.Formatter('[%(asctime)s] %(message)s')
+sh.setFormatter(cfm)
+dh.setFormatter(ffm)
+ih.setFormatter(ffm)
+
+log.addHandler(sh)
+log.addHandler(dh)
+log.addHandler(ih)
+log.info("Logger initialized")
+
+tl = logging.getLogger('trades')
+tl.setLevel(logging.DEBUG)
+tfh = logging.FileHandler('trades.log')
+tsh = logging.StreamHandler()
+tfh.setLevel(logging.DEBUG)
+tsh.setLevel(logging.INFO)
+tff = logging.Formatter('[%(asctime)s][%(levelname)s][%(name)s] %(message)s')
+tsf = logging.Formatter('[%(asctime)s] %(message)s')
+tfh.setFormatter(tff)
+tsh.setFormatter(tsf)
+tl.addHandler(tfh)
+tl.addHandler(tsh)
+tl.info("Trade Logger Initialized")
+
+def report_trade(action, ordersize, stacked):
+	report_string="action: %s\tordersize: %d\tstacked: %d" % (action, ordersize, stacked)
+	tl.info(report_string)
+	log.debug(report_string)
+	send_sms(report_string)
+
+
+def m15hist_positive(exchgdata):
+	# m15 for the div detection
+	(t, o, h, l, c, v) = exchgdata.get_split_tohlcv('15m', 34)
+	exchgdata.dprint_last_candles('15m', 5)
+	(m15kvo, m15signal) = klinger_kama(h, l, c, v)
+	m15hist = m15kvo[-1] - m15signal[-1]
+	log.info("m15hist is %.2f" % m15hist)	
+	positive = True
+	if m15hist <= 0:
+		positive = False
+	return positive
+
+sleeptime = 30
+
+send_sms('Trading Bot Active!')		
+
+# main loop
+bfxdata = ExchgData.ExchgData('bfx')
+
+last_hist_positive = m15hist_positive(bfxdata)
+hist_positive = last_hist_positive
+
+while [ 1 ]:
+	log.debug("Main loop")
+	mexorders.update_bracket_pct(config.sl, config.tp)
+
+	hist_positive = m15hist_positive(bfxdata)
+
+	shorts = mexorders.get_position_size('short')
+	longs = mexorders.get_position_size('long')
+	# if h3 kvo has flipped, flip positions
+	if hist_positive and not last_hist_positive:
+		mexorders.cancel_open_orders()
+		time.sleep(1)
+		mexorders.smart_order('Buy', shorts+config.ordersize)
+		report_trade("GOING LONG", shorts+config.ordersize, longs+config.ordersize)
+	elif not hist_positive and last_hist_positive:
+		mexorders.cancel_open_orders()
+		time.sleep(1)
+		mexorders.smart_order('Sell', longs+config.ordersize)
+		report_trade("GOING SHORT", longs+config.ordersize, shorts+config.ordersize)
+
+	last_hist_positive = hist_positive
+
+	# now print some status info
+	mexorders.print_positions()
+	mexorders.print_open_orders()
+	totalbal = mexorders.get_balance_total()
+	freebal = mexorders.get_balance_free()
+	log.info("Total Balance: %.4f, Free Balance: %.4f" % (totalbal, freebal))
+	log.info( "Loop completed, sleeping for %d seconds" % sleeptime)
+	time.sleep(sleeptime)
+
